1de617faf72331c8cd46420949fd725169e4bfa3
Tigris Mendez <70561045+uploadtigris@users.noreply.github.com>
2021-12-18 19:29:41 -0600

    Update Palindrome_Checker.cpp

--- Palindrome_Checker.cpp
+++ Palindrome_Checker.cpp
@@ -1,10 +1,49 @@
-// WORK IN PROGRESS
+
+// not all original code!
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        //if neg or NAN
+        if(x < 0)
+            return false;
+
+        //declare some vars
+        long long y = 0, t = x;
+        while (t != 0) {
+            y = y * 10 + t % 10;
+            t = (t/10);
+        }
+        return x == y;
+    }
+};
+int main() {
+    int x;
+    std::cout << "Hello, Welcome to the solved palindrome checker." << std::endl;
+    std::cout << "Please enter an integer" << std::endl;
+
+    cin >> x;
+    Solution called_func;
+
+    if(called_func.isPalindrome(x))
+    {
+        cout << "is palindrome";
+    }
+    else
+        cout << "not palindrome";
+
+    return 0;
+}
 
 
+// WORK IN PROGRESS (below)
+/*
 #include <iostream>
 #include <string>
 using namespace std;
-
 
 class Solution {
 public:
@@ -58,3 +97,5 @@ int main() {
     // compare the current array index value with the index array value of array[length-checked];
         // if equivalent, return true;
         // if not, retrun false;
+        
+       
